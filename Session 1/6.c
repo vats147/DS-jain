fe1bd648e9371f20f33bfffa3617063d914e37b3
vats <vatsalmangukiya9003@gmail.com>
2023-08-31 21:27:42 +0530

    Done 6

---
+++ Session 1/6.c
@@ -0,0 +1,27 @@
+/*
+       Author: Vatsal Mangukiya
+       Date: 31st aug,2023
+
+       Question 6: C program to calculate simple interest.
+*/
+
+#include<stdio.h>
+
+
+
+int main(){
+
+      int p,n;
+      float r;
+
+       p=10000;
+       n=5;
+       r=6;
+
+       float Simple_Interst;
+       Simple_Interst=p*r*n/100;   
+       printf("Simple interest is %f",Simple_Interst);
+      
+
+}
+

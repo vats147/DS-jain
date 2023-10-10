0089fe390527a034da62fa081f534383bd54d39a
vats <vatsalmangukiya9003@gmail.com>
2023-10-10 11:17:19 +0530

    Update 43.c and 44.c

--- Session 8/43.c
+++ Session 8/43.c
@@ -4,30 +4,24 @@
        Author: Vatsal Mangukiya
        Date: 2nd October,2023
 
-       Question:42.	C program to sort a list of numbers using Insertion sort.
+       Question:42.	43.	C program to find search for a given number using Linear search
 
 
 
 */
 #include <stdio.h>
-int binarySearch(int arr[],int low,int high,int find_number)
+int linersearch(int arr[],int find_number)
 {
-       while (low<=high)
-       {
-              int mid= low + (high-low )/2;
-              if(arr[mid]==find_number)
+      for (int i = 0; i < sizeof(arr); i++)
+      {
+              if(arr[i]==find_number)
               {
-                     return mid;
+                     printf("ARR[%d] == is %d",i,arr[i]);
+                     return i;
+
               }
-              if(arr[mid]<find_number)
-              {
-                     low=mid+1;
-              }
-              else
-              {
-                     high=mid-1;
-              }
-       }
+      }
+      
        return -1;
        
 }
@@ -35,23 +29,23 @@ int main()
 {
 
        // Declare variables
-       int n;
+       
        int max, min, minindex = 0;
 
        int find_number;
 
-       // Get the number of elements in the array
-       printf("Please Enter Number:");
-       scanf("%d", &n);
+       
 
        // Declare the array
        int arr[] = {2, 10, 30, 40, 50};
 
-       printf("Please enter number which you want to change");
+       printf("Please enter number which you want to find");
        scanf("%d", &find_number);
-       n = sizeof(arr) / sizeof(arr[0]);
-       int result = binarySearch(arr, 0, n - 1, find_number);
 
+       
+       int result = linersearch(arr, find_number);
+       printf("Value of result is : %d",result);
+       
        (result == -1) ? printf("Element is not present in array")
                       : printf("Element is present at index %d", result);
 
